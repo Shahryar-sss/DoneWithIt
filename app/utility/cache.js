6fb547ab18d4efe54791e152787d127421f5e77b
Shahryar-sss <saify.shahryar@gmail.com>
2020-08-30 22:18:05 +0530

    GET request-response data caching enabled

---
+++ app/utility/cache.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import moment from 'moment';
+
+const prefix = 'cache';
+const expiryInMinutes = 5;
+
+const isExpired = item => {
+    const now = moment(Date.now());
+    const storedTime = moment(item.timestamp);
+    return now.diff(storedTime, 'minutes') > expiryInMinutes;
+}
+
+const store = async (key, value) => {
+    try {
+        const item = {
+            value,
+            timestamp: Date.now()
+        }
+        await AsyncStorage.setItem(prefix + key, JSON.stringify(item));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+const get = async(key) => {
+    try {
+        const value = await AsyncStorage.getItem(prefix + key);
+        const item = JSON.parse(value);
+
+        if (!item) return null;
+
+        if (isExpired(item)) {
+            await AsyncStorage.removeItem(prefix+key);
+            return null;
+        }
+
+        return item.value;
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export default {
+    store,
+    get
+}
